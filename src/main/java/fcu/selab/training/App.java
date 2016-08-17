567ea14cc253c3808916cb9658e835f8cc74ff96
hsiu0929 <k83142917@gmail.com>
2016-08-17 20:35:36 +0800

    8/17 OK

--- src/main/java/fcu/selab/training/App.java
+++ src/main/java/fcu/selab/training/App.java
@@ -1,5 +1,9 @@
 package fcu.selab.training;
-
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
 /**
  * Hello world!
  *
@@ -8,6 +12,38 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        //System.out.println( "Hello World!" );
+        String line,temp = null;
+		String[] tempArray = new String[10];
+		
+		try{
+			FileReader fr = new FileReader("D:\\git\\input.txt");
+			BufferedReader br = new BufferedReader(fr);
+			
+			while((br.ready())){
+				temp = br.readLine();
+			}
+			
+			tempArray = temp.split(",");
+			
+			int lengh;
+			lengh = tempArray.length;	
+			
+			Arrays.sort(tempArray);
+			
+			FileWriter fw = new FileWriter("D:\\git\\output.txt");
+			
+			for(int i=0;i<lengh;i++){
+				fw.write(tempArray[i].toLowerCase().substring(0,1) + ". " + tempArray[i] + " ");
+			}
+			
+			fw.flush();
+			fw.close();
+			
+			System.out.println("OK");
+		}
+		catch(IOException e){
+			System.out.println("Fail");
+		}
     }
 }
